5e2105941da0a20e3fdd6032772befaf673074a1
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-03 17:09:43 -0300

    feat(AuditableEntity): add a AuditableEntity to a common/persistence

---
+++ src/main/java/dev/pedrorodrigo/tickethub/common/persistence/AuditableEntity.java
@@ -0,0 +1,30 @@
+package dev.pedrorodrigo.tickethub.common.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class AuditableEntity {
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
