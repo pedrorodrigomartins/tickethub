b62294b2a7d27c1cddb1c9999e9aa419d275a130
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-04 22:32:08 -0300

    feat(UserRepository): create a repository with methods

---
+++ src/main/java/dev/pedrorodrigo/tickethub/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package dev.pedrorodrigo.tickethub.user.repository;
+
+import dev.pedrorodrigo.tickethub.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
