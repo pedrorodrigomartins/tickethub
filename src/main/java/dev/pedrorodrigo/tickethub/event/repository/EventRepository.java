616f8b71233ac2a1a9c405e8151cb67932d7f94e
pedrorodrigo <pedrorodrigo.mdasilva@gmail.com>
2026-08-04 22:29:34 -0300

    feat(EventRepository): add methods

--- src/main/java/dev/pedrorodrigo/tickethub/event/repository/EventRepository.java
+++ src/main/java/dev/pedrorodrigo/tickethub/event/repository/EventRepository.java
@@ -1,10 +1,16 @@
 package dev.pedrorodrigo.tickethub.event.repository;
 
 import dev.pedrorodrigo.tickethub.event.entity.Event;
+import dev.pedrorodrigo.tickethub.event.enums.EventStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-    
+
+    List<Event> findByOrganizerId(Long organizerId);
+
+    List<Event> findByStatus(EventStatus status);
 }
